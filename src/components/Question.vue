cbfaeb62d921463c78c62f868a048a791523c65f
Muhammad Nadzhif Fikri <nadzhiff@gmail.com>
2026-01-13 19:40:54 +0700

    quiz questions

---
+++ src/components/Question.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+    question: Object,
+});
+</script>
+
+<template>
+  <section class="question">
+    <h3>{{ (question.id + 1) + ". " + question.question }}</h3>
+    <label v-for="option in question.options">
+        <input type="radio" :name="'q_' + question.id" :value="option.id">
+        {{ option.text }}
+    </label>
+  </section>
+</template>
+
+<style scoped>
+    label {
+        display: block;
+        position: relative;
+        width: 100%;
+    }
+
+    .question {
+        padding: 1rem;
+    }
+
+    .question  h3 {
+        font-size: 1.5rem;
+        color: #15394a;
+        font-weight: 800;
+    }
+
+    .question > label {
+        box-sizing: border-box;
+        margin: 0.5rem;
+        border: 0;
+        border-radius: 14px;
+        background-color: #016493;
+        padding: 1rem;
+        font-size: 1.2rem;
+        color: #FFFFFF;
+    }
+
+    .question > label:hover {
+        cursor: pointer;
+        background-color: #F3B124;
+    }
+
+    .question > label:has(input:checked) {
+        background-color: #F3B124;
+    }
+</style>
--- src/pages/Quiz.vue
+++ src/pages/Quiz.vue
@@ -1,30 +1,101 @@
 <script setup>
 import Header from "@/components/Header.vue";
+import Question from "@/components/Question.vue";
 
+const questions = [
+  {
+    id: 0,
+    question: "Siapa nama lengkap Miko?",
+    options: [
+      { id: 0, text: "Mikoplastik." },
+      { id: 1, text: "Mikoservis." },
+      { id: 2, text: "Mikola Syabila" },
+      { id: 3, text: "Mikosayang Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." }, 
+    ],
+    answer: 2,
+  },
+  {
+    id: 1,
+    question: "Siapa nama lengkap Miko?",
+    options: [
+      { id: 0, text: "Mikoplastik." },
+      { id: 1, text: "Mikoservis." },
+      { id: 2, text: "Mikola Syabila" },
+      { id: 3, text: "Mikosayang" },
+    ],
+    answer: 2,
+  },
+  {
+    id: 2,
+    question: "Siapa nama lengkap Miko?",
+    options: [
+      { id: 0, text: "Mikoplastik." },
+      { id: 1, text: "Mikoservis." },
+      { id: 2, text: "Mikola Syabila" },
+      { id: 3, text: "Mikosayang" },
+    ],
+    answer: 2,
+  },
+  {
+    id: 3,
+    question: "Siapa nama lengkap Miko?",
+    options: [
+      { id: 0, text: "Mikoplastik." },
+      { id: 1, text: "Mikoservis." },
+      { id: 2, text: "Mikola Syabila" },
+      { id: 3, text: "Mikosayang" },
+    ],
+    answer: 2,
+  },
+];
 
+function submitQuiz() {
+  for (question in questions) {
+    
+  }
+}
 
 </script>
 
 <template>
   <Header />
   <div id="join-wrapper">
-    <main>
-      
-    </main>
-    <Navbar />
+    <form @submit.prevent="submitQuiz">
+      <Question
+        v-for="question in questions"
+        :question="question"
+        :key="question.id"
+      />
+      <input type="submit" id="submit-btn">
+    </form>
   </div>
 </template>
 
 <style scoped>
-#join-wrapper {
-  width: 100%;
-}
+  #join-wrapper {
+    display: flex;
+    justify-content: center;
+  }
 
-main {
-  width: 50%;
-}
+  form {
+    width: 50%;
+    display: flexbox;
+  }
 
-#navbar {
-  width: 20%;
-}
+  #submit-btn {
+    width: 60%;
+    height: 5rem;
+    border: 2px solid ;
+    border-radius: 14px;
+    position: relative;
+    left: 20%;
+    margin-bottom: 5rem;
+    background-color: #5DA2CD;
+    color: white;
+  }
+
+  #submit-btn:hover {
+    background-color: #F3B124;
+    cursor: pointer;
+  }
 </style>
